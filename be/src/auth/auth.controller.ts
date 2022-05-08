a23fe8b555e9273de84e2e8a97c2d0eb759652be
Jan Laskaj <jan.laskaj@ipsos.com>
2022-05-08 13:38:02 +0200

    update db

--- be/src/auth/auth.controller.ts
+++ be/src/auth/auth.controller.ts
@@ -8,12 +8,11 @@ export class AuthController {
 
     @Post('signup')
     signUp(@Body() dto: AuthDto) {
-        console.log({ dto })
         return this.authService.signUp(dto)
     }
 
     @Post('signin')
-    signIn() {
-        return this.authService.signIn()
+    signIn(@Body() dto: AuthDto) {
+        return this.authService.signIn(dto)
     }
 }
--- be/src/auth/auth.service.ts
+++ be/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
 import * as argon from 'argon2'
 import { PrismaService } from '../prisma/prisma.service'
 import { AuthDto } from './dto'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
 
 @Injectable()
 export class AuthService {
@@ -10,16 +11,38 @@ export class AuthService {
     async signUp(dto: AuthDto) {
         const hash = await argon.hash(dto.password)
 
-        const user = await this.prisma.user.create({
-            data: { email: dto.email, hash },
-        })
+        try {
+            const user = await this.prisma.user.create({
+                data: { email: dto.email, hash },
+            })
 
-        delete user.hash
+            delete user.hash
 
-        return user
+            return user
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                // duplicate email
+                if ((error.code = 'P2002')) {
+                    throw new ForbiddenException('Credentials taken')
+                }
+            }
+
+            throw error
+        }
     }
 
-    signIn() {
-        return 'signin'
+    async signIn(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: { email: dto.email },
+        })
+
+        if (!user) throw new ForbiddenException('Credentials incorrect')
+
+        const pwdMatch = await argon.verify(user.hash, dto.password)
+
+        if (!pwdMatch) throw new ForbiddenException('Credentials incorrect')
+
+        delete user.hash
+        return user
     }
 }
