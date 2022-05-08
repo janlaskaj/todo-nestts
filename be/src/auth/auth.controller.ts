9053940c97e23270d186653a605bd1a4fe3f5a40
Jan Laskaj <jan.laskaj@ipsos.com>
2022-05-08 12:40:35 +0200

    add basic auth

--- be/src/auth/auth.controller.ts
+++ be/src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post } from '@nestjs/common'
+import { Controller, Post, Body } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { AuthDto } from './dto'
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signUp() {
-        return this.authService.signUp()
+    signUp(@Body() dto: AuthDto) {
+        console.log({ dto })
+        return this.authService.signUp(dto)
     }
 
     @Post('signin')
--- be/src/auth/auth.service.ts
+++ be/src/auth/auth.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common'
+import * as argon from 'argon2'
 import { PrismaService } from '../prisma/prisma.service'
+import { AuthDto } from './dto'
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
-    signUp() {
-        return 'signup'
+    async signUp(dto: AuthDto) {
+        const hash = await argon.hash(dto.password)
+
+        const user = await this.prisma.user.create({
+            data: { email: dto.email, hash },
+        })
+
+        delete user.hash
+
+        return user
     }
 
     signIn() {
