3950ce2c19c54ddb4afa6e83124f094f6c6f0584
Jan Laskaj <jan.laskaj@ipsos.com>
2022-05-14 07:45:54 +0200

    update strategy

--- be/src/auth/strategy/jwt.strategy.ts
+++ be/src/auth/strategy/jwt.strategy.ts
@@ -2,10 +2,11 @@ import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { ConfigService } from '@nestjs/config'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../../prisma/prisma.service'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor(private config: ConfigService) {
+    constructor(private config: ConfigService, private prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
@@ -13,7 +14,11 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
         })
     }
 
-    validate(payload: any) {
-        return payload
+    async validate(payload: { sub: number; email: string }) {
+        const user = await this.prisma.user.findUnique({
+            where: { email: payload.email },
+        })
+        delete user.hash
+        return user
     }
 }
