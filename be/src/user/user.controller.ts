13464b98569d3f8c87ee71a1d39ac6fce3a06d70
Jan Laskaj <jan.laskaj@ipsos.com>
2022-05-18 22:31:39 +0200

    update

--- be/src/user/user.controller.ts
+++ be/src/user/user.controller.ts
@@ -1,21 +1,32 @@
 import {
+    Body,
     Controller,
     Get,
     HttpCode,
     HttpStatus,
+    Patch,
     UseGuards,
 } from '@nestjs/common'
 import { User } from '@prisma/client'
 
 import { JwtGuard } from '../auth/guard'
 import { GetUser } from '../auth/decorator/'
+import { EditUserDto } from './dto'
+import { UserService } from './user.service'
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService) {}
     @HttpCode(HttpStatus.OK)
     @Get()
     getMe(@GetUser() user: User) {
         return user
+    }
+
+    @Patch()
+    async editUser(@GetUser() user: User, @Body() dto: EditUserDto) {
+        const user2 = await this.userService.editUser(user.id, dto)
+        return user2
     }
 }
---
+++ be/src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+import { EditUserDto } from './dto'
+
+@Injectable()
+export class UserService {
+    constructor(private prisma: PrismaService) {}
+    async editUser(userId: number, dto: EditUserDto) {
+        try {
+            const user = await this.prisma.user.update({
+                where: { id: userId },
+                data: {
+                    ...dto,
+                },
+            })
+
+            delete user.hash
+            return user
+        } catch (e) {
+            throw new BadRequestException(e)
+        }
+    }
+}
