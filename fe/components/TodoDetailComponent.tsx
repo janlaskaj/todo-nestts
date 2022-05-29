6fed19d3bafb29e7d14d59e1acb2652f1e286caf
Jan Laskaj <jan.laskaj@ipsos.com>
2022-05-29 12:58:58 +0200

    refactor styles

--- fe/components/TodoDetailComponent.tsx
+++ fe/components/TodoDetailComponent.tsx
@@ -6,9 +6,9 @@ type Props = {
 }
 
 export const TodoDetailComponent: React.FC<Props> = ({ todo }) => (
-    <div className="flex items-end justify-between p-2 border shadow flex-grow flex-shrink gap-4">
+    <div className="flex flex-shrink flex-grow items-end justify-between gap-4 border p-2 shadow">
         <div className="flex flex-col gap-2">
-            <h3 className="font-bold text-xl underline text-fuchsia-600">
+            <h3 className="text-xl font-bold text-fuchsia-600 underline">
                 {todo.title}
             </h3>
             <span>{todo.description}</span>
